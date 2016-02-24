52db32663168d1c0a5695b373db503546b932735
shivani14 <shivanisjoshi14@gmail.com>
2016-02-24 17:07:11 +0530

    login form validation
    login form validation..

--- www/js/login/login.controller.js
+++ www/js/login/login.controller.js
@@ -1,5 +1,21 @@
-angular.module('FamilyPlusApp').controller('loginController',['$scope','$location','$rootScope',function($scope,$location,$rootScope)
+angular.module('FamilyPlusApp').controller('loginController',['$scope','$location','$rootScope','$window',function($scope,$location,$rootScope,$window)
 {
 
      $rootScope.formCheck = {};
+// Validation for login
+
+     $scope.user ={};
+  
+
+     $scope.send = function()
+     {
+     		$scope.user={};
+ 
+
+     		$window.location.href="#/mainPage/home";
+     	
+     }
+    
+     
+
 }]);
