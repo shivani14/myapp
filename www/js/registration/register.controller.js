5e7eda97d1fe119a91cb4be1755c861caa3d402b
shivani14 <shivanisjoshi14@gmail.com>
2016-03-01 22:15:07 +0530

    validation
    validation with issues

--- www/js/registration/register.controller.js
+++ www/js/registration/register.controller.js
@@ -1,4 +1,27 @@
 angular.module('FamilyPlusApp').controller('registerController',['$scope',function($scope)
 {
 
+	$scope.checked=false;
+	$scope.user ={ };
+	$scope.err = "";
+
+	$scope.check = function()
+	{
+		if($scope.user.groupname==undefined)
+		{
+			$scope.err = "field is required";
+			console.log("--data-",false);
+			return false;
+		}
+		else
+		{
+			$scope.err = "";
+			return true;
+		}
+	}
+	$scope.send = function(value)
+	{
+		$scope.value1 = value;
+		console.log("--data--",$scope.value1 + "hii");
+	}
 }]);
