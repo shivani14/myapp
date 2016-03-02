29ffc19e76ad6961f4f1cd88a7cecb2960b672da
shivani14 <shivanisjoshi14@gmail.com>
2016-03-02 18:47:00 +0530

    inserting registration data
    used express js to store data on server side..

--- www/js/registration/register.controller.js
+++ www/js/registration/register.controller.js
@@ -1,4 +1,4 @@
-angular.module('FamilyPlusApp').controller('registerController',['$scope',function($scope)
+angular.module('FamilyPlusApp').controller('registerController',['$scope','$http'function($scope,$http)
 {
 
 	$scope.checked=false;
@@ -7,21 +7,51 @@ angular.module('FamilyPlusApp').controller('registerController',['$scope',functi
 
 	$scope.check = function()
 	{
-		if($scope.user.groupname==undefined)
+		if($scope.checked == false && $scope.user.groupname == undefined || $scope.user.groupname == "")
 		{
-			$scope.err = "field is required";
-			console.log("--data-",false);
+			$scope.err ="field is required";
+			console.log("--mydata--",$scope.err);
+			return false;
+		}
+		else if($scope.checked==true && $scope.user.groupname == undefined || $scope.user.groupname == "")
+		{
+			$scope.err ="field is required";
+			console.log("--mydata--",$scope.err);
+			return false;
+		}
+		else if($scope.checked==true && $scope.user.familyid == undefined || $scope.user.familyid == "")
+		{
+			$scope.err ="field is required";
+			console.log("--mydata--",$scope.err);
 			return false;
 		}
 		else
 		{
-			$scope.err = "";
+			$scope.err ="";
 			return true;
 		}
+
 	}
-	$scope.send = function(value)
+
+
+	$scope.send = function()
 	{
-		$scope.value1 = value;
-		console.log("--data--",$scope.value1 + "hii");
+		$http({
+			method :'post',
+			url : 'http://localhost:3001/';
+			data : $scope.user;
+			headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
+
+		}).success(function(data)
+		{
+			if(data.error)
+			{
+
+			}
+			else
+			{
+				console.log('--data--',"seneded");
+			}
+		});
 	}
 }]);
